0786531c901eb334f03d69450190e526743a069a
xkendrickz <kendrickchandra128@gmail.com>
2023-05-18 22:10:41 +0700

    ui login

--- app/src/main/java/com/example/p3l_android/MainActivity.kt
+++ app/src/main/java/com/example/p3l_android/MainActivity.kt
@@ -1,11 +1,197 @@
 package com.example.p3l_android
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.p3l_android.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.gson.Gson
+import org.json.JSONObject
+import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputUsername: TextInputLayout
+    private lateinit var inputPassword: TextInputLayout
+    private lateinit var etUsername: TextInputEditText
+    private lateinit var etPassword: TextInputEditText
+    private val myPreference = "myPref"
+    lateinit var mbundle : Bundle
+    lateinit var vUsername : String
+    lateinit var vPassword : String
+//    lateinit var checkUsername : String
+//    lateinit var checkPassword : String
+
+    private var layoutLoading: LinearLayout? = null
+    private var queue: RequestQueue? = null
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getSupportActionBar()?.hide();
+        setTitle("User Login")
+
+        queue = Volley.newRequestQueue(this)
+        layoutLoading = findViewById(R.id.layout_loading)
+        inputUsername = findViewById(R.id.inputLayoutUsername)
+        inputPassword = findViewById(R.id.inputLayoutPassword)
+        val textBtnSignUp : TextView = findViewById(R.id.textBtnSignUp)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        getBundle()
+        setText()
+
+        textBtnSignUp.setOnClickListener {
+            val moveRegister = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(moveRegister)
+        }
+
+        btnLogin.setOnClickListener(View.OnClickListener {
+            var checkLogin = false
+
+
+            if (inputUsername.getEditText()?.getText().toString().isEmpty()) {
+                inputUsername.setError("Username must be filled with text")
+            }else{
+                inputUsername.setError(null)
+            }
+
+            if (inputPassword.getEditText()?.getText().toString().isEmpty()) {
+                inputPassword.setError("Password must be filled with text")
+            }else{
+                inputPassword.setError(null)
+            }
+            if(inputUsername.getError() == null && inputPassword.getError() == null) checkLogin = true
+            if(inputUsername.getEditText()?.getText().toString() == "admin" && inputPassword.getEditText()?.getText().toString() == "admin"){
+                val moveAdmin = Intent(this@MainActivity, AdminActivity::class.java)
+                startActivity(moveAdmin)
+            }else if(!checkLogin) {
+                return@OnClickListener
+            }else{
+                login()
+            }
+
+//            if (!checkLogin) return@OnClickListener
+//            else{
+//
+//            }
+        })
+    }
+
+    private fun login() {
+        setLoading(true)
+        val user = Auth(
+            inputUsername.getEditText()?.getText().toString(),
+            inputPassword.getEditText()?.getText().toString()
+        )
+        val stringRequest: StringRequest =
+            object : StringRequest(Method.POST, UserApi.LOGIN, Response.Listener { response ->
+                val gson = Gson()
+                var user = gson.fromJson(response, User::class.java)
+
+                if(user != null)
+                    MotionToast.createToast(this@MainActivity,
+                        "Hurray success",
+                        "BERHASIL LOGIN",
+                        MotionToastStyle.SUCCESS,
+                        MotionToast.GRAVITY_BOTTOM,
+                        MotionToast.LONG_DURATION,
+                        ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+                val sharedPreference =  getSharedPreferences(myPreference, Context.MODE_PRIVATE)
+                var editor = sharedPreference.edit()
+                editor.putString("username",inputUsername.getEditText()?.getText().toString())
+                editor.apply()
+
+                val moveHome = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(moveHome)
+                setLoading(false)
+            }, Response.ErrorListener { error ->
+                setLoading(false)
+                try {
+                    val responseBody = String(error.networkResponse.data, StandardCharsets.UTF_8)
+                    val errors = JSONObject(responseBody)
+                    MotionToast.createToast(this@MainActivity,
+                        "Failed",
+                        errors.getString("message"),
+                        MotionToastStyle.ERROR,
+                        MotionToast.GRAVITY_BOTTOM,
+                        MotionToast.LONG_DURATION,
+                        ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                }
+
+            }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["Accept"] = "application/json"
+                    return headers
+                }
+
+                @Throws(AuthFailureError::class)
+                override fun getBody(): ByteArray {
+                    val gson = Gson()
+                    val requestBody = gson.toJson(user)
+                    return requestBody.toByteArray(StandardCharsets.UTF_8)
+                }
+
+                override fun getBodyContentType(): String {
+                    return "application/json"
+                }
+            }
+
+        // Menambahkan request ke request queue
+        queue!!.add(stringRequest)
+    }
+
+    fun getBundle(){
+        try{
+            mbundle = intent?.getBundleExtra("register")!!
+            if(mbundle != null){
+                vUsername = mbundle.getString("username")!!
+                vPassword = mbundle.getString("password")!!
+            }else{
+
+            }
+        }catch (e: NullPointerException){
+            vUsername = ""
+            vPassword = ""
+        }
+    }
+
+    fun setText() {
+        etUsername = findViewById(R.id.etUsername)
+        etUsername.setText(vUsername, TextView.BufferType.EDITABLE)
+        etPassword = findViewById(R.id.etPassword)
+        etPassword.setText(vPassword, TextView.BufferType.EDITABLE)
+    }
+
+    private fun setLoading(isLoading: Boolean) {
+        if (isLoading) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            )
+            layoutLoading!!.visibility = View.VISIBLE
+        } else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+            layoutLoading!!.visibility = View.GONE
+        }
     }
 }
